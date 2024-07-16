1ff383ef52fdd4aa0b9829c3cdfdc396a5a19ba4
vandanasaharan <22bcae64@kristujayanti.com>
2024-07-16 08:23:11 +0530

    Nodejs express CRUD operations with MongoDB

--- app.js
+++ app.js
@@ -1,63 +1,86 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json()); //Middleware to parse JSON requests
 
-let users = []; //List of the users
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/userDB', {});
 
-const createUserFunction = (req, res) => {
-    const user = req.body;
-    users.push(user);
-    res.status(201).send(user);
-}
+// Define User schema and model
+const User = mongoose.model('User', {
+    id: { type: String, unique: true, required: true }, // String,
+    name: String,
+    class: String,
+});
 
 // Create a new user
-// Endpoint, High Order function
-app.post('/users', createUserFunction);
-
-const getUserFunction = function(req, res){
-    console.log(users);
-    res.send(users);
-}
+app.post('/users', async (req, res) => {
+    const user = new User(req.body);
+    console.log(user);
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
 
 // Get all users
-app.get('/users', getUserFunction);
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.send(users);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
 // Get a user by ID
-app.get('/users/:id', (req, res) => {
-    const user = users.find(u => u.id === req.params.id);
-    if (user) {
-        res.send(user);
-    } else {
-        res.status(404).send({ message: 'User not found' });
+app.get('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findOne({ id: req.params.id });
+        if (user) {
+            res.send(user);
+        } else {
+            res.status(404).send({ message: 'User not found' });
+        }
+    } catch (err) {
+        res.status(500).send(err);
     }
 });
 
 // Update a user by ID
-app.put('/users/:id', (req, res) => {
-    const userIndex = users.findIndex(u => u.id === req.params.id);
-    if (userIndex !== -1) {
-        users[userIndex] = req.body;
-        res.send(users[userIndex]);
-    } else {
-        res.status(404).send({ message: 'User not found' });
+app.put('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findOneAndUpdate({ id: req.params.id }, req.body, { new: true });
+        if (user) {
+            res.send(user);
+        } else {
+            res.status(404).send({ message: 'User not found' });
+        }
+    } catch (err) {
+        res.status(400).send(err);
     }
 });
 
 // Delete a user by ID
-app.delete('/users/:id', (req, res) => {
-    const userIndex = users.findIndex(u => u.id === req.params.id);
-    if (userIndex !== -1) {
-        const deletedUser = users.splice(userIndex, 1);
-        res.send(deletedUser[0]);
-    } else {
-        res.status(404).send({ message: 'User not found' });
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findOneAndDelete({ id: req.params.id });
+        if (user) {
+            res.send(user);
+        } else {
+            res.status(404).send({ message: 'User not found' });
+        }
+    } catch (err) {
+        res.status(400).send(err);
     }
 });
 
 app.listen(port, () => {
-    console.log(`Hello, Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
